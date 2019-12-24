73e97048a8c666fce8f957a40d0a6eaf29948cd4
Steve Johnson <s.johnson-contractor@cablelabs.com>
2019-12-24 11:39:45 -0700

    wip install debugging

--- deploy/bin/_desktop.sh
+++ deploy/bin/_desktop.sh
@@ -11,6 +11,8 @@ echo "[Desktop Entry]" > $desktop_file
 echo "Type=Application" >> $desktop_file
 echo "Name=Micronets Proto-Pi" >> $desktop_file
 
-cmd="/usr/bin/python3 $(echo $PWD | rev | cut -d'/' -f3- | rev)/python3/proto_pi.py"
+thisdir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+cmd="/usr/bin/python3 $(echo $thisdir | rev | cut -d'/' -f3- | rev)/python3/proto_pi.py"
 
 echo "Exec=$cmd" >> $desktop_file
