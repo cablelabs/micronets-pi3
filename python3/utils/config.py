d7f87afaad16851118d0828fd1d9925a8cd873cc
Steve Johnson <s.johnson-contractor@cablelabs.com>
2019-12-25 13:32:19 -0700

    fixed config get/set recursion issues

--- python3/utils/config.py
+++ python3/utils/config.py
@@ -12,166 +12,159 @@ filename = os.path.join(folder, '../../config/config.json')
 
 class Config():
 
-    def __init__(self):
+	def __init__(self):
 
-        self.load_config()
-        self.set_defaults()
-        self.save_config()
+		self.load_config()
+		self.set_defaults()
+		self.save_config()
 
-    def config_default(self, key, default):
+	def config_default(self, key, default):
 
-        if not self.get(key):
-            self.set(key, default)
+		if not self.get(key):
+			self.set(key, default)
 
-    def save_config(self):
-        #with open(filename, 'w') as outfile:  
-        #    json.dump(self.config, outfile, sort_keys=True, indent=4, separators=(',', ': '))
-        self.dump(filename)
+	def save_config(self):
+		#with open(filename, 'w') as outfile:  
+		#    json.dump(self.config, outfile, sort_keys=True, indent=4, separators=(',', ': '))
+		self.dump(filename)
 
-    def set_defaults(self):
-        # config defaults
-        self.config_default('mode', 'dpp')
-        self.config_default('splashAnimationSeconds', 10)
-        self.config_default('onboardAnimationSeconds', 5)
-        self.config_default('messageTimeoutSeconds', 45)
+	def set_defaults(self):
+		# config defaults
+		self.config_default('mode', 'dpp')
+		self.config_default('splashAnimationSeconds', 10)
+		self.config_default('onboardAnimationSeconds', 5)
+		self.config_default('messageTimeoutSeconds', 45)
 
-        # clinic mode specific
-        self.config_default('registration_server', 'https://alpineseniorcare.com/micronets')
-        self.config_default('device_profile', 'device-0')
+		# clinic mode specific
+		self.config_default('registration_server', 'https://alpineseniorcare.com/micronets')
+		self.config_default('device_profile', 'device-0')
 
-        # dpp mode specific
-        self.config_default('vendorCode', "DAWG")
-        self.config_default('channel', 1)
-        self.config_default('channelClass', 81)
-        self.config_default('qrcode_countdown', 30)
-        # proxy settings are used when simulating the iphone mobile application (click on qrcode)
-        self.config_default(['dppProxy','msoPortalUrl'], "https://mso-portal-api.micronets.in")
-        self.config_default(['dppProxy','username'], "grandma")
-        self.config_default(['dppProxy','password'], "grandma")
-        self.config_default(['dppProxy','deviceModelUID'], "AgoNDQcDDgg")
+		# dpp mode specific
+		self.config_default('vendorCode', "DAWG")
+		self.config_default('channel', 1)
+		self.config_default('channelClass', 81)
+		self.config_default('qrcode_countdown', 30)
+		# proxy settings are used when simulating the iphone mobile application (click on qrcode)
+		self.config_default(['dppProxy','msoPortalUrl'], "https://mso-portal-api.micronets.in")
+		self.config_default(['dppProxy','username'], "grandma")
+		self.config_default(['dppProxy','password'], "grandma")
+		self.config_default(['dppProxy','deviceModelUID'], "AgoNDQcDDgg")
 
-        # customization flags
-        self.config_default('demo', True)
-        self.config_default('comcast', False)
+		# customization flags
+		self.config_default('demo', True)
+		self.config_default('comcast', False)
 
-    def load_config(self):
-        try:
-            with open(filename, 'r') as infile:  
-                file_data = infile.read()
-                self.config = json.loads(file_data)
+	def reset(self):
+		self.config = {}
 
-                logger.info("config loaded OK: "+str(len(self.config))+ " keys")
-                #logger.info("\n" + json.dumps(config))
+	def load_config(self):
+		try:
+			with open(filename, 'r') as infile:  
+				file_data = infile.read()
+				self.config = json.loads(file_data)
 
-        except (KeyError, FileNotFoundError) as e:
-            logger.info("config load error")
+				logger.info("config loaded OK: "+str(len(self.config))+ " keys")
+				#logger.info("\n" + json.dumps(config))
 
-            # Start empty, create boilerplate from defaults.
-            self.config = {}
-            pass
+		except (KeyError, FileNotFoundError) as e:
+			logger.info("load_config: config file does not exist. Default will be generated")
 
-    def dump(self, file=None):
-        if not file:
-            return json.dumps(self.config,  sort_keys=True, indent=4, separators=(',', ': '))
-        else:
-            with open(file, 'w') as outfile:  
-                json.dump(self.config, outfile, sort_keys=True, indent=4, separators=(',', ': '))
+			# Start empty, create boilerplate from defaults.
+			self.config = {}
+			pass
 
-    # get/set methods are recursive
-    def set(self, key, value, dictionary=None):
+	def dump(self, file=None):
+		if not file:
+			return json.dumps(self.config,  sort_keys=True, indent=4, separators=(',', ': '))
+		else:
+			with open(file, 'w') as outfile:  
+				json.dump(self.config, outfile, sort_keys=True, indent=4, separators=(',', ': '))
 
-        if not dictionary:
-            dictionary = self.config
+	def set(self, key, value):
 
-        if isinstance(key, list):
-            # array of keys
-            k0 = key.pop(0)
-            if not k0 in dictionary:
-                # This is an insert
-                if len(key) == 0:
-                    # no more keys, insert value
-                    dictionary[k0] = value
-                else:
-                    dictionary[k0] = {}
-                    self.set(key, value, dictionary[k0])
-            else:
-                # This is an update
-                x = dictionary[k0]
-                if isinstance(x, dict):
-                    self.set(key, value, x)
-                else:
-                    dictionary[k0] = value
-        else:
-            # single key, update or insert
-            dictionary[key] = value
+		if isinstance(key, list):
 
-    def get(self, key, default=None, dictionary=None):
+			obj = self.config
 
-        if not dictionary:
-            dictionary = self.config
+			i = 0
+			while i < (len(key)-1):
+				k = key[i]
+				if not k in obj or not isinstance(obj[k], dict):
+					obj[k] = {}
+				obj = obj[k]
+				i += 1
 
-        if isinstance(key, list):
-            # array of keys
-            k0 = key.pop(0)
-            if k0 in dictionary:
-                x = dictionary[k0]
-                if isinstance(x, dict):
-                    return self.get(key, default, x)
-                else:
-                    return x
-            else:
-                return default if default else None
-        else:
-            # single key
-            if key in dictionary:
-                return dictionary[key]
-            else:
-                return default if default else None
+			k = key[len(key)-1]
+			obj[k] = value
+
+		else:
+			# single key, update or insert
+			self.config[key] = value
+
+	def get(self, key, default=None):
+		if isinstance(key, list):
+			obj = self.config
+			i = 0
+			while i < (len(key)-1):
+				k = key[i]
+				if not k in obj or not isinstance(obj[k], dict):
+					return default if default else None
+				obj = obj[k]
+				i += 1
+
+			k = key[len(key)-1]
+			if k in obj:
+				return obj[k]
+			else:
+				return default if default else None
+		else:
+			# single key
+			if key in self.config:
+				return self.config[key]
+			else:
+				return default if default else None
 
 # The one and only thing that gets imported with "from utils.config import *"
 config = Config()
-        
+		
 if __name__ == '__main__':
 
-    # !!! You MUST run this from the parent directory: python3 -m utils.config
-    # !!! - note the -m switch and no .py extension
+	# !!! You MUST run this from the parent directory: python3 -m utils.config
+	# !!! - note the -m switch and no .py extension
+	# start empty
+	config.reset()
 
-    _config = Config()
+	# initial values
+	logger.info("countdown: "+str(config.get('countdown')))
+	logger.info("[dppProxy][password]: "+str(config.get(['dppProxy','password'])))
 
-    # initial values
-    logger.info("countdown: "+str(_config.get('countdown')))
-    logger.info("[dppProxy][password]: "+str(_config.get(['dppProxy','password'])))
+	# update
+	config.set("countdown", 809)
+	config.set(['dppProxy','password'], 'flerb')
 
-    # update
-    _config.set("countdown", 809)
-    _config.set(['dppProxy','password'], 'flerb')
+	# modified values
+	logger.info("countdown: "+str(config.get('countdown')))
+	logger.info("[dppProxy][password]: "+str(config.get(['dppProxy','password'])))
 
-    # modified values
-    logger.info("countdown: "+str(_config.get('countdown')))
-    logger.info("[dppProxy][password]: "+str(_config.get(['dppProxy','password'])))
+	# fetch non-existent
+	logger.info("aaa: "+str(config.get('aaa')))
+	logger.info("[bbb][ccc]: "+str(config.get(['bbb','ccc'])))
 
-    # fetch non-existent
-    logger.info("aaa: "+str(_config.get('aaa')))
-    logger.info("[bbb][ccc]: "+str(_config.get(['bbb','ccc'])))
+	# fetch non-existent with defaults
+	logger.info("aaa: "+str(config.get('aaa', 'xyz')))
+	logger.info("[bbb][ccc]: "+str(config.get(['bbb','ccc'], 'tbd')))
 
-    # fetch non-existent with defaults
-    logger.info("aaa: "+str(_config.get('aaa', 'xyz')))
-    logger.info("[bbb][ccc]: "+str(_config.get(['bbb','ccc'], 'tbd')))
+	# insert new key value pairs
+	config.set("aaa", 888)
+	config.set(['bbb','ccc'], 'grimp')
 
-    # insert new key value pairs
-    _config.set("aaa", 888)
-    _config.set(['bbb','ccc'], 'grimp')
+	# fetch inserted values
+	logger.info("aaa: "+str(config.get('aaa')))
+	logger.info("[bbb][ccc]: "+str(config.get(['bbb','ccc'])))
 
-    # fetch inserted values
-    logger.info("aaa: "+str(_config.get('aaa')))
-    logger.info("[bbb][ccc]: "+str(_config.get(['bbb','ccc'])))
+	# dump JSON
+	config.dump("dump.json")
+	logger.info(config.dump())
 
-    # dump JSON
-    _config.dump(os.getenv("HOME")+"/dump.json")
-    logger.info(_config.dump())
-
-    logger.info("comcast: "+str(_config.get("comcast")))
-
-    logger.info(_config.get(['dppProxy','msoPortalUrl']))
 
 
