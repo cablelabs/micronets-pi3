18c5094c14002cd4761c7c028b2bfcd3ad58ea24
Steve Johnson <s.johnson-contractor@cablelabs.com>
2019-12-31 11:04:27 -0700

    removed debug flag from _wpas_logfile. Use wpa_cli log_file instead

--- deploy/bin/_wpas_logfile.sh
+++ deploy/bin/_wpas_logfile.sh
@@ -1,21 +1,23 @@
 #!/bin/bash
-# Set wpa_supplicant logging (with debug output) to logfile: /tmp/wpa_supplicant.log
+# Set wpa_supplicant logging to logfile: /tmp/wpa_supplicant.log
 
 LOGFILE=${1:-"on"}
 
 if [ "$LOGFILE" == "on" ]; then
-    echo "Setting wpa_supplicant logging w/debug to /tmp/wpa_supplicant.log"
-    sudo sed -i 's|wpa_supplicant -B|wpa_supplicant -f /tmp/wpa_supplicant.log -d -B|' "/lib/dhcpcd/dhcpcd-hooks/10-wpa_supplicant"
+    echo "Setting wpa_supplicant logging to /tmp/wpa_supplicant.log"
+    sudo sed -i 's|wpa_supplicant -B|wpa_supplicant -f /tmp/wpa_supplicant.log -B|' "/lib/dhcpcd/dhcpcd-hooks/10-wpa_supplicant"
+    echo "Use sudo wpa_cli log_level [DEBUG | INFO] to turn debug output on/off"
 elif [ "$LOGFILE" == "off" ]; then
-    echo "Setting wpa_supplicant logging w/o debug to syslog"
-    sudo sed -i 's|wpa_supplicant -f /tmp/wpa_supplicant.log -d -B|wpa_supplicant -B|' "/lib/dhcpcd/dhcpcd-hooks/10-wpa_supplicant"
+    echo "Setting wpa_supplicant logging to syslog"
+    sudo sed -i 's|wpa_supplicant -f /tmp/wpa_supplicant.log -B|wpa_supplicant -B|' "/lib/dhcpcd/dhcpcd-hooks/10-wpa_supplicant"
+    echo "Use sudo wpa_cli log_level [DEBUG | INFO] to turn debug output on/off"
 else
     echo "usage: _wpas_logfile.sh [on/off]   # default is on"
 fi
 
 # Restart?
-read -r -p "Reboot Now? [y/N] " response
+read -r -p "Restart wpa_supplicant Now? [y/N] " response
 if [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]]
 then
-	sudo reboot 
+	sudo systemctl restart wpa_supplicant@wlan0.service
 fi
