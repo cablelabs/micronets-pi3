6734d7bf3f44ace8411c8277c7816b5847560fae
Steve Johnson <s.johnson-contractor@cablelabs.com>
2020-01-09 12:36:58 -0700

    fixed splash screens

--- deploy/files/splash.sh
+++ deploy/files/splash.sh
@@ -1,14 +1,6 @@
 #!/bin/bash
-# Have not found a reliable "After" unit to specify in the service file in order to show the splash
 
-counter=0
-while [ $counter -lt 40 ]
-do
-  if [ -w /dev/fb1 ]; then
-  	/usr/bin/fbi -T 2 -d /dev/fb1 -noverbose -a /usr/local/images/splash.png  2> /dev/null
-  	break
-  else
-     counter=$(expr $counter + 1)
-  fi
-  sleep .1s
-done
+/usr/bin/fbi -T 2 -d /dev/fb1 -noverbose -a /usr/local/images/splash.png  2> /dev/null
+
+# without the sleep, the service/script exits before the frame buffer is written to.
+sleep 2
